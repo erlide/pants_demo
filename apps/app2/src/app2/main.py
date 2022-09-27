58bfbda70a1f21976cf8f3b70fcd6c8039c7ce1b
Vlad Dumitrescu <vlad.dumitrescu@volvocars.com>
2022-09-27 15:38:32 +0200

    pex updates

--- apps/app2/src/app2/main.py
+++ apps/app2/src/app2/main.py
@@ -2,12 +2,19 @@
 
 import rich
 from lib2 import lib2_m
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: int
+    name = "Jane Doe"
 
 
 def mmain():
     """mmain."""
     rich.print("app2")
     lib2_m.util_2()
+    print(User(id=3))
 
 
 if __name__ == "__main__":
